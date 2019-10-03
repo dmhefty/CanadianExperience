2ff05328d65271c969d7aa2ee60d6dedefb942a7
akhil <alluriak@msu.edu>
2019-10-03 12:42:18 -0700

    Added declerations for functions OnDraw and Load. Also added pointers for player and scoreboard objects.

--- Project1/Game.h
+++ Project1/Game.h
@@ -1,5 +1,35 @@
 #pragma once
+#include "pch.h"
+#include <string>
+#include <memory>
+#include "Harold.h"
+#include "ScoreBoard.h"
 class CGame
 {
+private:
+/// Game area width in virtual pixels
+	const static int Width = 1250;
+/// Game area height in virtual pixels
+	const static int Height = 1000;
+
+//pointer to the player object
+	std::unique_ptr<CHarold> mPlayer;
+
+//pointer to the scoreboard object
+	std::unique_ptr<CScoreBoard> mScoreBoard;
+
+public:
+/**
+* Draw the game area
+* \param graphics The GDI+ graphics context to draw on
+* \param width Width of the client window
+* \param height Height of the client window
+*/
+	void OnDraw(Gdiplus::Graphics* graphics, int width, int height);
+	
+	
+	void Load(std::string filePath);
+
+
 };
 
