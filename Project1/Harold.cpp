53b14846df716c1cac0968ed2cc38f950d1ca9cb
Isaac A Mayers <mayersis@msu.edu>
2019-10-03 11:06:37 -0400

    Added Empty Harold Class

---
+++ Project1/Harold.cpp
@@ -0,0 +1,7 @@
+/**
+ * \file Harold.cpp
+ *
+ * \author Isaac Mayers
+ */
+#include "pch.h"
+#include "Harold.h"
---
+++ Project1/Harold.h
@@ -0,0 +1,17 @@
+/**
+ * \file Harold.h
+ *
+ * \author Isaac Mayers
+ *
+ * Class for the character, Harold
+ */
+
+
+#pragma once
+ /**
+  * Harold class
+  */
+class CHarold
+{
+};
+
