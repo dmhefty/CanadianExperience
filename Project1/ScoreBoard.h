18aae50ed3a04f9f8359725edda6d025cb821b2f
prasadj2 <prasadj2@msu.edu>
2019-10-02 23:12:58 -0400

    Added empty ScoreBoard class.

---
+++ Project1/ScoreBoard.h
@@ -0,0 +1,18 @@
+/**
+ * \file ScoreBoard.h
+ *
+ * \author Jaideep Prasad
+ *
+ * Class for the game score board
+ */
+
+#pragma once
+
+/**
+ * Game score board class
+ */
+class CScoreBoard
+{
+
+};
+
