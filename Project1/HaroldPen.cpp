94116f1353de7aaa381e4b03c470c40886615317
David Hefty <heftydav@msu.edu>
2019-10-03 20:45:55 -0400

    Work on CItem+CHaroldPen draw functions.

--- Project1/HaroldPen.cpp
+++ Project1/HaroldPen.cpp
@@ -7,6 +7,11 @@
 #pragma once
 #include "pch.h"
 #include "HaroldPen.h"
+#include <string>
+using namespace std;
+
+ /// Pen filename 
+const wstring PEN_IMAGE_NAME = L"images/redpen.png";
 
  /**
   * \brief Constructor for CHaroldPen class
@@ -14,8 +19,14 @@
   * \param position takes in a position in the form of a CVector
   * \param velocity takes in a velocity in the form of a CVector
   */
-CHaroldPen::CHaroldPen(CVector position, CVector velocity) : CItem(position, velocity)
+CHaroldPen::CHaroldPen(CVector position, CVector velocity) : CItem(position, velocity, PEN_IMAGE_NAME)
 {
+	mIsAttached = true;
+}
+
+void CHaroldPen::Draw(Gdiplus::Graphics* graphics, CVector position)
+{
+	CItem::Draw(graphics, position);
 }
 
 
--- Project1/Item.cpp
+++ Project1/Item.cpp
@@ -7,7 +7,12 @@
 #pragma once
 #include "pch.h"
 #include "Item.h"
+#include <string>
+#include <vector>
+#include <memory>
 using namespace std;
+using namespace Gdiplus;
+
 
 /**
  * \brief Constructor for CItem class
@@ -15,8 +20,15 @@ using namespace std;
  * \param position takes in a position in the form of a CVector
  * \param velocity takes in a velocity in the form of a CVector
  */
-CItem::CItem(CVector position, CVector velocity) : mPosition(position), mVelocity(velocity)
+CItem::CItem(CVector position, CVector velocity, wstring imageLocation) : mPosition(position), mVelocity(velocity)
 {
+	mItemImage = unique_ptr<Bitmap>(Bitmap::FromFile(imageLocation.c_str()));
+	if (mItemImage->GetLastStatus() != Ok)
+	{
+		wstring msg(L"Failed to open ");
+		msg += imageLocation;
+		AfxMessageBox(msg.c_str());
+	}
 }
 
 /**
@@ -41,6 +53,25 @@ void CItem::SetVelocity(CVector velocity)
 	mVelocity = tempVel;
 }
 
+
+/**
+* Draw this item
+* \param graphics The graphics context to draw on
+* \param x X location to draw in virtual pixels
+* \param y Y location to draw in virtual pixels
+* \param angle Angle of rotation in radians
+*/
+void CItem::Draw(Gdiplus::Graphics* graphics, CVector position)
+{
+	float wid = (float)mItemImage->GetWidth();
+	float hit = (float)mItemImage->GetHeight();
+
+	auto state = graphics->Save();
+	graphics->TranslateTransform((float)position.X(), (float)position.Y());
+	graphics->DrawImage(mItemImage.get(), -wid / 2, -hit / 2,
+		wid, hit);
+	graphics->Restore(state);
+}
 
 /**
  * \brief Updates the velocity based on how long has passed since the last update
