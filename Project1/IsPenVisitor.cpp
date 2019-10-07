ffb704c3c2b88e30bc5d594860dcdb728ddb5b79
Reid Shinabarker <shinab10@msu.edu>
2019-10-06 22:21:10 -0400

    Created IsPenVisitor class, but code is commented out due to current lack of itemVisitor base class

---
+++ Project1/IsPenVisitor.cpp
@@ -0,0 +1,15 @@
+/**
+ * \file IsPenVisitor.cpp
+ *
+ * \author Reid Shinabarker
+ */
+
+#include "pch.h"
+#include "IsPenVisitor.h"
+
+/** \brief Visit a CHaroldPen object
+* \param pen Pen item we are visiting */
+//void CIsPenVisitor::VisitPen(CHaroldPen* pen)
+//{
+//	mIsPen = true;
+//}
