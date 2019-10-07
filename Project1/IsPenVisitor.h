ea0b6eeab806cd7feded8217cd6ef6eb4b54654a
Jaideep Prasad <prasadj2@msu.edu>
2019-10-07 17:27:57 -0400

    Merge

---
+++ Project1/IsPenVisitor.h
@@ -0,0 +1,30 @@
+/**
+* \file IsPenVisitor.h
+*
+* \author Reid Shinabarker
+*
+* Pen concrete visitor class.
+*/
+
+
+#pragma once
+//#include "ItemVisitor.h"
+//#include "HaroldPen.h"
+//
+///** Pen concrete visitor */
+//class CIsPenVisitor :
+//	public CItemVisitor
+//{
+//public:
+//
+//	virtual void VisitPen(CHaroldPen* pen) override;
+//
+//	/** Tells the visitor that this is a pen item
+//	* \returns True if the item is a pen */
+//	bool IsPen() { return mIsPen; }
+//
+//private:
+//	/// Bool for if this is a pen item or not
+//	bool mIsPen = false;
+//};
+
