46cba14fe43acd82b0526ed9d53a3404d9fc4f69
Isaac A Mayers <mayersis@msu.edu>
2019-10-18 18:31:39 -0400

    Final

--- Project1/ScoreBoard.cpp
+++ Project1/ScoreBoard.cpp
@@ -32,13 +32,13 @@ CScoreBoard::~CScoreBoard()
 void CScoreBoard::Draw(Gdiplus::Graphics* graphics)
 {
 	FontFamily fontFamily(L"Arial");
-	Gdiplus::Font font(&fontFamily, 16);
+	Gdiplus::Font font(&fontFamily, 26);
 
-	SolidBrush green(Color(200, 250, 200));
-	graphics->DrawString(to_wstring(mCorrect).c_str(), -1, &font, PointF(-575, 50), &green);
-	graphics->DrawString(L"Correct", -1, &font, PointF(-650, 100), &green);
-	graphics->DrawString(to_wstring(mMissed).c_str(), -1, &font, PointF(-25, 50), &green);
-	graphics->DrawString(L"Missed", -1, &font, PointF(-100, 100), &green);
-	graphics->DrawString(to_wstring(mUnfair).c_str(), -1, &font, PointF(500, 50), &green);
-	graphics->DrawString(L"Unfair", -1, &font, PointF(450, 100), &green);
+	SolidBrush black(Color(0, 0, 0));
+	graphics->DrawString(to_wstring(mCorrect).c_str(), -1, &font, PointF(-575, 50), &black);
+	graphics->DrawString(L"Correct", -1, &font, PointF(-650, 100), &black);
+	graphics->DrawString(to_wstring(mMissed).c_str(), -1, &font, PointF(-25, 50), &black);
+	graphics->DrawString(L"Missed", -1, &font, PointF(-100, 100), &black);
+	graphics->DrawString(to_wstring(mUnfair).c_str(), -1, &font, PointF(500, 50), &black);
+	graphics->DrawString(L"Unfair", -1, &font, PointF(450, 100), &black);
 }
