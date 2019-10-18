a65e55297b8eb4e5b1953dd6435be95d88ca5ef0
Reid Shinabarker <shinab10@msu.edu>
2019-10-18 19:10:26 -0400

    Documented UML.h

--- Project1/UML.h
+++ Project1/UML.h
@@ -1,3 +1,11 @@
+/**
+ * \file UML.h
+ *
+ * \author team Sarson 
+ *
+ * Class for the actual UML object
+ */
+
 #pragma once
 #include "pch.h"
 #include <memory>
@@ -10,28 +18,34 @@ class CUML :
 	public CItem
 {
 private:
+	/// number of attributes on the object
 	int mNumberOfAttributes;
 
-	//pointer to the object for the name assigned to this uml item
+	/// pointer to the object for the name assigned to this uml item
 	std::shared_ptr<CUMLAttribute> mName;
 	
-	//vector of 0-3 attributes associated with this uml object
+	/// vector of 0-3 attributes associated with this uml object
 	std::vector<std::shared_ptr<CUMLAttribute> > mAttributes;
 
-	//vector of 0-3 operations associated with this uml object
+	/// vector of 0-3 operations associated with this uml object
 	std::vector<std::shared_ptr<CUMLAttribute> > mOperations;
 
+	/// width of the uml object
 	double mWidth = -1;
 
+	/// height of the uml object
 	double mHeight = -1;
 
 public:
+	/// Accepts a visitor
+	/// \param visitor The visitor that is accessing this object
 	virtual void Accept(CItemVisitor* visitor) override { visitor->VisitUML(this); }
 
 	virtual void Update(double elapsedTime) override;
 
 	virtual void Draw(Gdiplus::Graphics* graphics, CVector position) override;
 
+	/// Getter for the dimensions of the UML object
 	virtual CVector GetDimensions() const override
 	{
 		return CVector(mWidth, mHeight);
@@ -43,16 +57,29 @@ public:
 		mDisplayLocation = CItem::GetPosition();
 	}
 
+	/// Getter for mDeleted variable
+	/// \returns mDeleted value
 	bool IsDeleted() const { return mDeleted; }
 
 protected:
 
+	/// constructor
+	/// \param name Name attribute
+	/// \param attributes List of attributes for the object
+	/// \param operations List of operations for the object
+	/// \param position Starting position of the object
+	/// \param velocity Starting velocity of the object
+	/// \param game The game object
 	CUML(std::shared_ptr<CUMLAttribute> name, std::vector<std::shared_ptr<CUMLAttribute> > attributes, 
 		std::vector<std::shared_ptr<CUMLAttribute> > operations, CVector position, CVector velocity, CGame* game);
 
+	/// bool for if the object has been hit by the pen
 	bool mHit = false;
+	/// bool for if the object is deleted
 	bool mDeleted = false;
+	/// time object has been on screen
 	double mTimeDisplayed = 0;
+	/// location of the object
 	CVector mDisplayLocation;
 
 };
